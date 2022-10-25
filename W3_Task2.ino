878e6e12bbb324f5c3e4df1f06841cc8df1c899d
mariamebeid73 <111026391+mariamebeid73@users.noreply.github.com>
2022-10-26 01:26:35 +0200

    Update W3_Task2.ino

--- W3_Task2.ino
+++ W3_Task2.ino
@@ -1,12 +1,13 @@
-#include<ros.h>
-#include<std_msgs/Int32.h>
+#include <ros.h>
 
-std_msgs::Int32 count_msg;
+#include <std_msgs/Float32.h>
+
 ros::NodeHandle nh;
-ros::Publisher encoder("Encoder",&count_msg);
+std_msgs::Float32 count_msg;
+ros::Publisher encoder("Encoder", &count_msg);
 
-#define pinA 21
-#define pinB 22
+#define pinA 2
+#define pinB 3
 
 int counter = 0;
 int publisher_timer = 0;
@@ -14,39 +15,37 @@ int publisher_timer = 0;
 
 void setup()
 {
+  Serial.begin(9600);
   nh.initNode();
-  nh.advertise( encoder );
-  pinMode(pinA,INPUT_PULLUP);
-  pinMode(pinB,INPUT_PULLUP);  
-
-  attachInterrupt(digitalPinToInterrupt(pinA),isrA,CHANGE);
-  attachInterrupt(digitalPinToInterrupt(pinB),isrB,CHANGE);
+//  nh.advertise(encoder);/
+//  pinMode(pinA, INPUT_PULLUP);
+//  pinMode(pinB, INPUT_PULLUP);
+//
+//  attachInterrupt(digitalPinToInterrupt(pinA), isrA, CHANGE);
+//  attachInterrupt(digitalPinToInterrupt(pinB), isrB, CHANGE);
 }
 
-void loop() 
+void loop()
 {
-  if(millis() > publisher_timer)
-  {
-    count_msg.data = counter;
-    encoder.publish(&count_msg);
-    publisher_timer = millis() + 1000;
-  }
+
+//  count_msg.data = counter;
+//  encoder.publish(&count_msg);
   nh.spinOnce();
-  
+
 }
 
 void isrA()
-{ 
-  if(digitalRead(pinA) != digitalRead(pinB))
+{
+  if (digitalRead(pinA) != digitalRead(pinB))
     counter ++;
-   else
+  else
     counter --;
 }
 
 void isrB()
 {
-  if(digitalRead(pinA) == digitalRead(pinB)) 
+  if (digitalRead(pinA) == digitalRead(pinB))
     counter ++;
   else
-    counter --; 
+    counter --;
 }
